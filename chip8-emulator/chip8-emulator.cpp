f6516e31c76ff418797b2fbf0c0a46cb4fde3cc1
Felix <sangsuchanx@gmail.com>
2024-08-06 21:43:04 +0200

    created the decoder and execution loop

--- chip8-emulator/chip8-emulator.cpp
+++ chip8-emulator/chip8-emulator.cpp
@@ -1,12 +1,49 @@
+#include <array>
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <windows.h>
+#include <stack>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
 const int SCALE_FACTOR = 10;
 const int EMULATOR_WIDTH = 64;
 const int EMULATOR_HEIGHT = 32;
+
+const int default_color_r = 255;
+const int default_color_g = 255;
+const int default_color_b = 255;
+
+enum registers
+{
+    V0 = 0x00,
+    V1 = 0x01,
+    V2 = 0x02,
+    V3 = 0x03,
+    V4 = 0x04,
+    V5 = 0x05,
+    V6 = 0x06,
+    V7 = 0x07,
+    V8 = 0x08,
+    V9 = 0x09,
+    VA = 0x0A,
+    VB = 0x0B,
+    VC = 0x0C,
+    VD = 0x0D,
+    VE = 0x0E,
+    VF = 0x0F /* flag register */
+};
+
+uint16_t read_2_bytes(std::vector<uint8_t>& memory, uint32_t index) {
+    if (index + 1 >= memory.size()) {
+        throw std::out_of_range("Index out of bounds!!!");
+    }
+    return (static_cast<uint16_t>(memory[index]) << 8) | static_cast<uint16_t>(memory[index + 1]);
+}
+
 
 int8_t init_sdl()
 {
@@ -44,6 +81,22 @@ int8_t set_pixel(Uint32* pixelData, int pitch, int x, int y, int r, int g, int b
 }
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) return 0;
+	/* prepare file operations */
+    std::ifstream input_file(argv[1], std::ios::binary | std::ios::ate);
+    const std::streamsize file_size = input_file.tellg();
+    input_file.seekg(0, std::ios::beg);
+
+    /* allocate the necessary vector size */
+	std::vector<uint8_t> file_content(file_size);
+
+    /* read the file! */ 
+    if(!input_file.read(reinterpret_cast<char*>(file_content.data()), file_size))
+    {
+        std::cerr << "Error while reading file\n";
+        return -1;
+    }
+
     /* init sdl */
     init_sdl();
     /* create window */
@@ -81,24 +134,72 @@ int main(int argc, char* argv[]) {
 
     Uint32* pixelData = static_cast<Uint32*>(pixels);
 
-    /*set pixels */
-    for (int i = 0; i < 600; ++i) {
-        set_pixel(pixelData, pitch, i, i, 255, 0, 0); // Red color
-    }
+    std::array<uint8_t, 16> registers;
 
-    SDL_UnlockTexture(sdl_texture);
-    SDL_RenderClear(sdl_renderer);
-    SDL_RenderCopy(sdl_renderer, sdl_texture, NULL, NULL);
-    SDL_RenderPresent(sdl_renderer);
+    int16_t register_PC = 0;
+    int16_t register_I = 0;
 
-    bool quit = false;
+    int8_t delay_timer = 0;
+    int8_t sound_timer = 0;
+
+    std::stack<uint16_t> program_stack;
+
     SDL_Event e;
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
+
+    uint16_t num = 0x00 + 0xe0;
+
+    while(register_PC + 1 < file_content.size())
+    {
+        SDL_PollEvent(&e);
+	    /* fetch instruction */
+        uint16_t instruction = file_content[register_PC] + file_content[register_PC + 1];
+        uint8_t inst_first = file_content[register_PC];
+        uint8_t inst_second = file_content[register_PC + 1];
+
+        /* retrieve the nibbles */
+        uint8_t nibble_1 = inst_first >> 4;
+        uint8_t nibble_2 = inst_first & 15;
+        uint8_t nibble_3 = inst_second >> 4;
+        uint8_t nibble_4 = inst_second & 15;
+
+        std::cout << "Instruction: 0x" << std::hex << inst_first << std::dec << std::endl;
+        std::cout << "Instruction 2 : 0x" << std::hex << inst_second << std::dec << std::endl;
+
+        uint16_t t_test = instruction & 0xF000; /* -> t_test will only contain data from first nibble */
+        uint16_t t_test2 = instruction & 0x00FF; /* -> only data from third and fourth nibble */
+
+        switch(instruction & 0xF000) 
+        {
+            /* clear screen */
+			case 0x0000:
+	        {
+                    switch(instruction & 0x00FF)
+                    {
+						case 0x00e0:
+                        {
+                            std::cout << "clearing screen" << std::endl;
+                            SDL_SetRenderDrawColor(sdl_renderer, 0, 0, 0, 0);
+                            SDL_RenderClear(sdl_renderer);
+                            SDL_RenderPresent(sdl_renderer);
+                            break;
+                        }
+						default:
+                        {
+                            break;
+                        }
+
+                    }
+	        }
+			default:
+            {
+					std::cout << "default case" << std::endl;
+					break;
             }
         }
+
+        register_PC += 2;
+        Sleep(2000);
+
     }
 
     /* try to destroy the created windows, textures etc.! */
