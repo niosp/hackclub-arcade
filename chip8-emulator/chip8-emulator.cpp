00b5f0d43d30e797cc17da0274f523ff2228fcdc
Felix <sangsuchanx@gmail.com>
2024-08-07 20:57:11 +0200

    debugging the ibm logo display

--- chip8-emulator/chip8-emulator.cpp
+++ chip8-emulator/chip8-emulator.cpp
@@ -7,9 +7,10 @@
 #include <stack>
 #include <vector>
 #include <fstream>
+#include <iomanip>
 #include <sstream>
 
-const int SCALE_FACTOR = 10;
+const int SCALE_FACTOR = 15;
 const int EMULATOR_WIDTH = 64;
 const int EMULATOR_HEIGHT = 32;
 
@@ -17,7 +18,30 @@ const int default_color_r = 255;
 const int default_color_g = 255;
 const int default_color_b = 255;
 
-enum registers
+const int entry_point = 0x00;
+
+uint8_t display[EMULATOR_WIDTH][EMULATOR_HEIGHT] = {0};
+
+const uint8_t font_data[80] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+enum register_enum
 {
     V0 = 0x00,
     V1 = 0x01,
@@ -36,6 +60,45 @@ enum registers
     VE = 0x0E,
     VF = 0x0F /* flag register */
 };
+
+bool is_pixel_black(SDL_Texture* texture, int x, int y)
+{
+    // Get texture format and size
+    Uint32 format;
+    int access, w, h;
+    SDL_QueryTexture(texture, &format, &access, &w, &h);
+
+    // Make sure the coordinates are within bounds
+    if (x < 0 || x >= w || y < 0 || y >= h) {
+        std::cerr << "Coordinates are out of bounds." << std::endl;
+        return false;
+    }
+
+    // Lock the texture
+    void* pixels;
+    int pitch;
+    if (SDL_LockTexture(texture, NULL, &pixels, &pitch) != 0) {
+        std::cerr << "Failed to lock texture: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    // Calculate the position of the pixel
+    Uint32* pixelArray = (Uint32*)pixels;
+    int pixelIndex = (y * pitch / 4) + x;
+    Uint32 pixelValue = pixelArray[pixelIndex];
+
+    // Unlock the texture
+    SDL_UnlockTexture(texture);
+
+    // Extract color components
+    Uint8 r, g, b, a;
+    SDL_PixelFormat* pixelFormat = SDL_AllocFormat(format);
+    SDL_GetRGBA(pixelValue, pixelFormat, &r, &g, &b, &a);
+    SDL_FreeFormat(pixelFormat);
+
+    // Check if the pixel is black (assuming black means all color components are 0)
+    return (r == 0 && g == 0 && b == 0);
+}
 
 uint16_t read_2_bytes(std::vector<uint8_t>& memory, uint32_t index) {
     if (index + 1 >= memory.size()) {
@@ -82,20 +145,52 @@ int8_t set_pixel(Uint32* pixelData, int pitch, int x, int y, int r, int g, int b
 
 int main(int argc, char* argv[]) {
     if (argc < 2) return 0;
+    /* print filename */
+    std::cout << argv[1] << "\n";
 	/* prepare file operations */
     std::ifstream input_file(argv[1], std::ios::binary | std::ios::ate);
     const std::streamsize file_size = input_file.tellg();
+    /* check filesize, since memory is limited */
+    if (file_size > 3584) return -1;
+    /* later, read from the beginning of the file stream */
     input_file.seekg(0, std::ios::beg);
-
     /* allocate the necessary vector size */
-	std::vector<uint8_t> file_content(file_size);
+    std::vector<uint8_t> file_content(4096);
 
     /* read the file! */ 
-    if(!input_file.read(reinterpret_cast<char*>(file_content.data()), file_size))
+    if(!input_file.read(reinterpret_cast<char*>(file_content.data() + entry_point), file_size))
     {
         std::cerr << "Error while reading file\n";
         return -1;
     }
+
+    /* close input file */
+
+    /* insert font data into memory */
+    // file_content.insert(file_content.begin() + 0x50, &font_data[0], &font_data[80]);
+
+
+    std::ifstream file(argv[1], std::ios::binary);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << argv[1] << std::endl;
+    }
+
+    unsigned char byte;
+    int count = 0;
+
+    while (file.read(reinterpret_cast<char*>(&byte), sizeof(byte))) {
+        std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(byte) << ' ';
+        if (++count % 16 == 0) { // New line every 16 bytes
+            std::cout << std::endl;
+        }
+    }
+
+    if (count % 16 != 0) { // Ensure to end with a new line if the last line is not complete
+        std::cout << std::endl;
+    }
+
+    file.close();
+
 
     /* init sdl */
     init_sdl();
@@ -124,6 +219,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
+    /*
     void* pixels;
     int pitch;
     if (SDL_LockTexture(sdl_texture, NULL, &pixels, &pitch) != 0) {
@@ -131,28 +227,40 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return 1;
     }
+    */
+    SDL_SetRenderDrawColor(sdl_renderer, 255, 0, 0, 255);
+    SDL_RenderClear(sdl_renderer);
+    SDL_RenderPresent(sdl_renderer);
 
-    Uint32* pixelData = static_cast<Uint32*>(pixels);
+    std::array<uint8_t, 16> registers = {0};
 
-    std::array<uint8_t, 16> registers;
-
-    int16_t register_PC = 0;
+    int16_t register_PC = entry_point;
     int16_t register_I = 0;
 
     int8_t delay_timer = 0;
     int8_t sound_timer = 0;
 
-    std::stack<uint16_t> program_stack;
+    std::stack<uint16_t> program_stack = {};
 
     SDL_Event e;
 
-    uint16_t num = 0x00 + 0xe0;
+    /*
+     * Instructions are from here: https://en.wikipedia.org/wiki/CHIP-8
+     * todo: implement the necessary instructions!
+     * 00E0: clear screen, done
+     * 1NNN: jump to NNN, done
+     * 00EE: return from subroutine, 
+     * 6XNN: set the register VX
+     *
+     *
+     *
+     */
 
     while(register_PC + 1 < file_content.size())
     {
         SDL_PollEvent(&e);
 	    /* fetch instruction */
-        uint16_t instruction = file_content[register_PC] + file_content[register_PC + 1];
+        uint16_t instruction = (file_content[register_PC] << 8) | file_content[register_PC + 1];
         uint8_t inst_first = file_content[register_PC];
         uint8_t inst_second = file_content[register_PC + 1];
 
@@ -162,44 +270,113 @@ int main(int argc, char* argv[]) {
         uint8_t nibble_3 = inst_second >> 4;
         uint8_t nibble_4 = inst_second & 15;
 
-        std::cout << "Instruction: 0x" << std::hex << inst_first << std::dec << std::endl;
-        std::cout << "Instruction 2 : 0x" << std::hex << inst_second << std::dec << std::endl;
-
-        uint16_t t_test = instruction & 0xF000; /* -> t_test will only contain data from first nibble */
-        uint16_t t_test2 = instruction & 0x00FF; /* -> only data from third and fourth nibble */
-
-        switch(instruction & 0xF000) 
+        /* switch over the first nibble */
+        switch(instruction & 0xF000)
         {
-            /* clear screen */
-			case 0x0000:
+        case 0x00:
 	        {
-                    switch(instruction & 0x00FF)
-                    {
-						case 0x00e0:
-                        {
-                            std::cout << "clearing screen" << std::endl;
-                            SDL_SetRenderDrawColor(sdl_renderer, 0, 0, 0, 0);
-                            SDL_RenderClear(sdl_renderer);
-                            SDL_RenderPresent(sdl_renderer);
-                            break;
-                        }
-						default:
-                        {
-                            break;
-                        }
-
-                    }
+                /* switch over third + fourth nibble */
+		        switch(instruction & 0x00FF)
+		        {
+		        case 0x00e0:
+			        {
+		        		std::cout << "clearing screen" << std::endl;
+						SDL_SetRenderDrawColor(sdl_renderer, 0, 0, 0, 0);
+		        		SDL_RenderClear(sdl_renderer);
+		        		SDL_RenderPresent(sdl_renderer);
+		        		break;
+			        }
+		        default:
+			        {
+						std::cout << "default case 1\n";
+		        		break;
+			        }
+		        }
+                break;
 	        }
-			default:
-            {
-					std::cout << "default case" << std::endl;
-					break;
-            }
+        case 0x1000:
+	        {
+        		std::cout << "jump instruction" << std::endl;
+                register_PC = instruction & 0x0FFF;
+                std::cout << "asdasd" << std::endl;
+                break;
+	        }
+        case 0x2000:
+	        {
+        		break;
+	        }
+        case 0x6000:
+	        {
+        		registers[nibble_2] = inst_second;
+        		break;
+	        }
+        case 0x7000:
+	        {
+				registers[nibble_2] += inst_second;
+        		break;
+	        }
+        case 0xA000:
+	        {
+				register_I = (instruction & 0x0FFF);
+                std::cout << "asd" << std::endl;
+        		break;
+	        }
+        case 0xD000:
+	        {
+        		const uint8_t x_coordinate = registers[nibble_2];
+                const uint8_t y_coordinate = registers[nibble_3];
+
+                std::cout << "X: " << (int)x_coordinate << " Y: " << (int)y_coordinate << "\n";
+
+                const uint8_t height = nibble_4;
+
+                registers[VF] = 0x00;
+
+                /* draw "row" rows until height is reached */
+                for(uint8_t row=0; row < height; row++)
+                {
+                    uint8_t sprite_data = file_content[register_I + row];
+                    /* draw 8 pixels for each row */
+	                for(uint8_t col=0; col < 8; col++)
+	                {
+                        uint8_t pixel_x = x_coordinate + col;
+                        uint8_t pixel_y = y_coordinate + row;
+
+                        uint8_t current_pixel = (sprite_data & (1 << col)) >> col;
+
+                        if(pixel_x >= 64 || pixel_y >= 32)
+                        {
+                            std::cout << "Out of range. Break.\n";
+                        }else
+                        {
+	                        if(current_pixel && display[pixel_x][pixel_y])
+	                        {
+                                registers[VF] = 0x01;
+	                        }else if(current_pixel && !(display[pixel_x][pixel_y]))
+	                        {
+                                SDL_Rect rectangle;
+                                rectangle.x = pixel_x;
+                                rectangle.y = pixel_y;
+                                rectangle.h = 1;
+                                rectangle.w = 1;
+
+                                SDL_SetRenderDrawColor(sdl_renderer, 0, 0, 255, 255);
+                                SDL_RenderFillRect(sdl_renderer, &rectangle);
+	                        	SDL_RenderPresent(sdl_renderer);
+	                        }
+                        }
+	                }
+                }
+        		break;
+	        }
+        default:
+	        {
+            std::cout << "default case 2\n";
+        		break;
+	        }
         }
 
         register_PC += 2;
-        Sleep(2000);
-
     }
 
     /* try to destroy the created windows, textures etc.! */
---
+++ chip8-emulator/test-roms/chip8-disassembler.py
@@ -0,0 +1,57 @@
+# from https://github.com/ASankaran/CHIP8-Disassembler/blob/master/chip8_disassemble.py
+import sys
+import binascii
+
+from opcode_table import opcodeTable
+from opcode_args_table import opcodeArgsTable
+
+
+def main() :
+	if len(sys.argv) < 2:
+		print("Incorrect arguements.")
+		exit()
+
+	filePath = sys.argv[1]
+	assemblyInstructions = generateAssembly(filePath)
+	for instruction in assemblyInstructions:
+		print(instruction)
+	
+
+def generateAssembly(filePath):
+	assemblyInstructions = []
+	with open(filePath, "rb") as f:
+		programCounter = 0x200
+		opcode = f.read(2)
+		while opcode:
+			hexOpcode = binascii.hexlify(opcode)
+			opcodeInstruction = lookupOpcode(int(hexOpcode, 16))
+			opcodeArgsList = findOpcodeArgs(int(hexOpcode, 16))
+			opcodeArgs = ""
+			for arg in opcodeArgsList:
+				opcodeArgs += arg + ", "
+			opcodeArgs = opcodeArgs[ : -2]
+			assemblyInstructions.append(hex(programCounter) + ":\t" + hexOpcode + "\t\t" + opcodeInstruction + "\t" + opcodeArgs)
+			programCounter += 2;
+			opcode = f.read(2)
+	return assemblyInstructions
+
+def maskOpcode(opcode):
+	if (opcode & 0xF000) in [0x0000, 0xE000, 0xF000]:
+		opcode &= 0xF0FF
+	elif (opcode & 0xF000) in [0x8000]:
+		opcode &= 0xF00F
+	else:
+		opcode &= 0xF000
+	return opcode
+
+def lookupOpcode(opcode):
+	opcode = maskOpcode(opcode)
+	return opcodeTable.get(opcode, "???");
+
+def findOpcodeArgs(opcode):
+	maskedOpcode = maskOpcode(opcode)
+	return opcodeArgsTable.get(maskedOpcode)(opcode)
+
+
+if __name__ == '__main__':
+	main()
